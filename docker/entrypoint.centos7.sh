cce4b978860d714953a9e004941ef0e8b1f4d480
Dan Petrisko <petrisko@cs.washington.edu>
2025-03-06 21:43:56 -0800

    Update git in entry script

--- docker/entrypoint.centos7.sh
+++ docker/entrypoint.centos7.sh
@@ -3,4 +3,7 @@ echo "Activating python venv"
 source /opt/venv/bin/activate
 echo "Activating devtoolset-11"
 source scl_source enable devtoolset-11
+echo "Activating git218"
+source scl_source enable rh-git218
+
 exec "$@"
