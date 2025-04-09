01dfbabf87657e51e49b3238ce733eafaa6872f4
Dan Petrisko <petrisko@cs.washington.edu>
2025-04-09 02:01:12 -0700

    Adding standard directories for upload and download

--- ci/smoke-verilator.sh
+++ ci/smoke-verilator.sh
@@ -2,13 +2,13 @@
 
 # get common functions
 source $(dirname $0)/common/functions.sh
-
-script_name=$(basename $0)
-
 bsg_log_info "starting $(basename $0)"
 
+# fail on any error
+set -e
+
 # check for binaries in path
-bsg_check_var "BP_INSTALL_DIR" || exit 1
+bsg_check_var "BP_INSTALL_DIR"
 bsg_log_info "setting installation directory as ${BP_INSTALL_DIR}"
 bsg_run_task "checking for installed binaries" ls ${BP_INSTALL_DIR}/bin
 PATH=${BP_INSTALL_DIR}/bin:${PATH}
@@ -16,7 +16,7 @@ PATH=${BP_INSTALL_DIR}/bin:${PATH}
 # do the actual job
 bsg_run_task "finding verilator" which verilator
 bsg_run_task "getting verilator version" verilator --version
-bsg_run_task "getting verilator help" verilator --help
 
-bsg_log_info "finishing $(basename $0)"
+# pass if no error
+bsg_pass $(basename $0)
 
--- ci/smoke-yosys.sh
+++ ci/smoke-yosys.sh
@@ -2,13 +2,13 @@
 
 # get common functions
 source $(dirname $0)/common/functions.sh
-
-script_name=$(basename $0)
-
 bsg_log_info "starting $(basename $0)"
 
+# fail on any error
+set -e
+
 # check for binaries in path
-bsg_check_var "BP_INSTALL_DIR" || exit 1
+bsg_check_var "BP_INSTALL_DIR"
 bsg_log_info "setting installation directory as ${BP_INSTALL_DIR}"
 bsg_run_task "checking for installed binaries" ls ${BP_INSTALL_DIR}/bin
 PATH=${BP_INSTALL_DIR}/bin:${PATH}
@@ -18,5 +18,6 @@ bsg_run_task "finding yosys" which yosys
 bsg_run_task "getting yosys version" yosys --version
 bsg_run_task "getting yosys help" yosys --help
 
-bsg_log_info "finishing $(basename $0)"
+# pass if no error
+bsg_pass $(basename $0)
 
