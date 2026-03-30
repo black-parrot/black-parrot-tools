be98e779c3851b5335e3247bbeb15de710bbc058
Dan Ruelas-Petrisko <petrisko.bsg@gmail.com>
2026-03-29 22:26:12 -0700

    Fix ci

--- ci/smoke-surelog.sh
+++ ci/smoke-surelog.sh
@@ -2,9 +2,9 @@
 source $(dirname $0)/functions.sh
 
 # do the actual job
-bsg_run_task "finding uhdm-dump" which uhdm-dump
+bsg_run_task "uhdm_find" "finding uhdm-dump" which uhdm-dump
 bsg_sentinel_fail "which: no"
-bsg_run_task "getting uhdm-dump version" uhdm-dump --version
+bsg_run_task "uhdm_version" "getting uhdm-dump version" uhdm-dump --version
 
 # pass if no error
 bsg_pass $(basename $0)
